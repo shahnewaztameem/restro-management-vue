baa9e2a6c6796985c604685ec469c6515203b4a9
Shahnewaz Tameem <shahnewaztamim@gmail.com>
2021-09-09 18:52:48 +0600

    signup and set user to localstorage

---
+++ src/components/SignUp.vue
@@ -0,0 +1,64 @@
+<template>
+  <img src="../assets/logo.png" alt="logo" />
+  <h1>Sign Up</h1>
+  <div class="register">
+    <input type="text" v-model="name" placeholder="Enter Name" />
+    <input type="email" v-model="email" placeholder="Enter Email" />
+    <input type="password" v-model="password" placeholder="Enter Password" />
+
+    <button @click="signUp" type="submit">Sign Up</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'SignUp',
+  data() {
+    return {
+      name: '',
+      email: '',
+      password: '',
+    }
+  },
+  methods: {
+    async signUp() {
+      let result = await axios.post('http://localhost:3000/users', {
+        email: this.email,
+        password: this.password,
+        name: this.name,
+      })
+
+      console.warn(result)
+
+      if(result.status === 201) {
+        alert('sign up success')
+        localStorage.setItem('user-info', JSON.stringify(result.data))
+      } 
+    },
+  },
+}
+</script>
+
+<style>
+.register input {
+  width: 300px;
+  height: 40px;
+  padding-left: 20px;
+  display: block;
+  margin-bottom: 30px;
+  margin-right: auto;
+  margin-left: auto;
+  border: 1px solid skyblue;
+}
+
+.register button {
+  width: 320px;
+  height: 40px;
+  border: 1px solid skyblue;
+  background: skyblue;
+  color: #fff;
+  cursor: pointer;
+}
+</style>
