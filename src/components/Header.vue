383d74fbd6b28376e2bfacd2e3dac68f5e0aca8f
Shahnewaz Tameem <shahnewaztamim@gmail.com>
2021-09-12 18:42:40 +0600

    Add and update page route, db config

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="nav">
-    <a href="#">Home</a>
-    <a href="#">Add Restaurant</a>
-    <a href="#">Update Restaurant</a>
-    <a href="#" @click="logout">Logout</a>
+    <router-link to="#">Home</router-link>
+    <router-link to="/add">Add Restaurant</router-link>
+    <router-link to="/update">Update Restaurant</router-link>
+    <router-link to="#" @click="logout">Logout</router-link>
   </div>
 </template>
 
--- src/components/Update.vue
+++ src/components/Update.vue
@@ -1,24 +1,18 @@
 <template>
   <Header />
-  <h1>Hello {{ name }}</h1>
+  <h1>Update restaurant page</h1>
 </template>
 
 <script>
 import Header from './Header'
 
 export default {
-  name: 'Home',
-  data() {
-    return {
-      name: '',
-    }
-  },
+  name: 'Update',
   components: {
     Header,
   },
   mounted() {
     let user = localStorage.getItem('user-info')
-    this.name = JSON.parse(user).name
 
     if (!user) {
       this.$router.push({ name: 'SignUp' })
