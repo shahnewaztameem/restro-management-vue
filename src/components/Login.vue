43201fcc1a9b2f98e00970ee6776ce5592375b8e
Shahnewaz Tameem <shahnewaztamim@gmail.com>
2021-09-12 14:57:44 +0600

    implement login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -2,11 +2,10 @@
   <img src="../assets/logo.png" alt="logo" />
   <h1>Login</h1>
   <div class="login">
-   
     <input type="email" v-model="email" placeholder="Enter Email" />
     <input type="password" v-model="password" placeholder="Enter Password" />
 
-    <button type="submit">Login</button>
+    <button @click="login" type="submit">Login</button>
     <p>
       <router-link to="/signup">Sign Up</router-link>
     </p>
@@ -14,7 +13,36 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'Login',
+  data() {
+    return {
+      email: '',
+      password: '',
+    }
+  },
+  methods: {
+    async login() {
+      let result = await axios.get(
+        `http://localhost:3000/users?email=${this.email}&password=${this.password}`
+      )
+
+      if (result.status === 200 && result.data.length > 0) {
+        localStorage.setItem('user-info', JSON.stringify(result.data[0]))
+        this.$router.push({ name: 'Home' })
+      }
+
+      console.log(result)
+    },
+  },
+  mounted() {
+    let user = localStorage.getItem('user-info')
+
+    if (user) {
+      this.$router.push({ name: 'Home' })
+    }
+  },
 }
 </script>
