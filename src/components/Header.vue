0519ccdd28c8159ba8a8ca9bbb6ed9620aea929c
Shahnewaz Tameem <shahnewaztamim@gmail.com>
2021-09-12 17:46:50 +0600

    Add logout functionality

---
+++ src/components/Header.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="nav">
+    <a href="#">Home</a>
+    <a href="#">Add Restaurant</a>
+    <a href="#">Update Restaurant</a>
+    <a href="#" @click="logout">Logout</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  methods: {
+    logout() {
+      localStorage.clear()
+      this.$router.push({name: 'Login'})
+    },
+  },
+}
+</script>
+
+<style>
+.nav {
+  background-color: #333;
+  overflow: hidden;
+}
+
+.nav a {
+  float: left;
+  color: #f2f2f2f2;
+  padding: 14px 16px;
+  text-align: center;
+  font-size: 17px;
+  text-decoration: none;
+  margin-right: 5px;
+  transition: 0.5s all ease;
+}
+
+.nav a:hover {
+  background-color: #ddd;
+  color: #333;
+}
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,16 @@
 <template>
+  <Header />
   <h1>Hello world</h1>
 </template>
 
 <script>
+import Header from './Header'
+
 export default {
   name: 'Home',
+  components: {
+    Header,
+  },
   mounted() {
     let user = localStorage.getItem('user-info')
 
